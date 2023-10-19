f18d79960e24d4f45793d6c01c046adbb78ec3e4
Vladyslav Diachenko <basher207vd@gmail.com>
2023-10-19 15:02:12 +0300

    Works with saving data of the checked tasks after reloading the page

--- app.js
+++ app.js
@@ -2,13 +2,54 @@ const express = require("express");
 const app = express();
 const ejsLayouts = require("express-ejs-layouts");
 
-//configuration
+app.set('view engine', 'ejs');
 
-const tasks = [];
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(ejsLayouts);
+
+const tasks = [
+    {
+        content: "testTask1",
+        checked: false
+    },
+    {
+        content: "testTask2",
+        checked: true
+    }
+];
 
 // Routes
+app.get("/", (req, res) => {
+    res.render('pages/index', {tasks: tasks});
+});
+app.post("/checkbox/:taskIndex", (req, res) => {
+    if(req.body.done){
+        tasks[req.params.taskIndex].checked = true;
+    }else{
+        tasks[req.params.taskIndex].checked = false;
+    }
+    res.redirect("/");
+});
+app.post("/add", (req, res) => { 
+    if(req.body.task){
+        tasks.push({
+            content: req.body.task,
+            checked: false
+        });
+    }
+    res.redirect("/");
+});
+app.post("/delete/:index", (req, res) => {
+    tasks.splice(req.params.index, 1);
+    res.redirect("/");
+});
 
 
-const server = //start the server
+// const server = //start the server
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+    console.log(`Server has been started on port: ${PORT}`);
+})
 
 module.exports = server;
